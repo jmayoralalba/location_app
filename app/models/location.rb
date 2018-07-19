135d8d19fb4636d5e9c286af6fd3221a20dc5cf0
Javier Mayoral Alba <jmayoralalba@gmail.com>
2018-07-19 12:02:58 +0200

    Override as_json to exclude timestamps and floats format

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,4 +7,13 @@ class Location < ApplicationRecord
   validates :country,   presence: true, length: { in: 1..255 }
   validates :latitude,  presence: true, numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }
   validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+
+  def as_json(options = {})
+    h = super(options.merge({ except: [ :created_at, :updated_at ] }))
+
+    h[:latitude]  = self.latitude.to_f
+    h[:longitude] = self.longitude.to_f
+
+    h
+  end
 end
